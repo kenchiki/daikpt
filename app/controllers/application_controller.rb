1d324ff5ada34d514ad78644b1b089ae2b8c900c
daikitakaya <takaya@sonicgarden.jp>
2019-03-26 02:42:25 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :require_project
-
-  def require_project
-    redirect_to new_project_path if current_user.projects.blank?
-  end
 end
