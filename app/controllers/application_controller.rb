9058c6985e74bcf7a7d99de3950908a5873b16cf
daikitakaya <takaya@sonicgarden.jp>
2019-03-26 02:22:47 +0900

    プロジェクト作成ページへリダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :require_project
+
+  def require_project
+    redirect_to new_project_path if current_user.project.nil?
+  end
 end
