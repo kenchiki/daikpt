ee4221c57b3cc038edbcdda23a52b1d998e4b80d
daikitakaya <takaya@sonicgarden.jp>
2019-03-26 20:18:47 +0900

    リファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'projects#index'
   devise_for :users
   resources :projects do
-    resources :kpts, except: %i(edit update destroy)
+    resources :kpts, only: %i(index new create)
   end
   resources :keep_things, only: %i(index)
   resources :try_things, only: %i(index)
