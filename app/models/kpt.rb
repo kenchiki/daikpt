929fdce83da088fbed0e6895cef3a92ce75594ec
daikitakaya <takaya@sonicgarden.jp>
2019-03-26 21:33:00 +0900

    typo修正

--- app/models/kpt.rb
+++ app/models/kpt.rb
@@ -2,8 +2,8 @@ class Kpt < ApplicationRecord
   has_many :keep_things, inverse_of: :kpt
   has_many :problem_things, inverse_of: :kpt
   has_many :try_things, through: :problem_things
-  accepts_nested_attributes_for :keep_things, allow_destroy: true
-  accepts_nested_attributes_for :problem_things, allow_destroy: true
+  accepts_nested_attributes_for :keep_things, reject_if: :all_blank
+  accepts_nested_attributes_for :problem_things, reject_if: :all_blank
   belongs_to :project
 
   def build_relation_things
--- app/models/problem_thing.rb
+++ app/models/problem_thing.rb
@@ -1,7 +1,7 @@
 class ProblemThing < ApplicationRecord
   has_many :try_things, inverse_of: :problem_thing
   belongs_to :kpt, optional: true
-  accepts_nested_attributes_for :try_things, allow_destroy: true
+  accepts_nested_attributes_for :try_things, reject_if: :all_blank
 
   validates :content, presence: :true
 end
