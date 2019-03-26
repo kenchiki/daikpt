6f6a3798866546bb92030a059d6e58d17be33cc6
daikitakaya <takaya@sonicgarden.jp>
2019-03-26 20:23:38 +0900

    バリデーション漏れを追加

--- app/models/problem_thing.rb
+++ app/models/problem_thing.rb
@@ -2,4 +2,6 @@ class ProblemThing < ApplicationRecord
   has_many :try_things, inverse_of: :problem_thing
   belongs_to :kpt, optional: true
   accepts_nested_attributes_for :try_things, allow_destroy: true
+
+  validates :content, presence: :true
 end
--- app/models/try_thing.rb
+++ app/models/try_thing.rb
@@ -1,3 +1,5 @@
 class TryThing < ApplicationRecord
   belongs_to :problem_thing, optional: true
+
+  validates :content, true
 end
