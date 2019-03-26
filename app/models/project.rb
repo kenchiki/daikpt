c79dd2b7be49dfb23915baaeea36fee50d7b5a29
daikitakaya <takaya@sonicgarden.jp>
2019-03-26 20:23:38 +0900

    バリデーション漏れを追加

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
   has_many :project_memberships, dependent: :destroy
   has_many :users, through: :project_memberships
   has_many :kpts, dependent: :destroy
+
+  validates :name, presence: true
 end
