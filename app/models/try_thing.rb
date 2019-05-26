e5502790e2b498e55846262f882c612e02d2c032
daikitakaya <takaya@sonicgarden.jp>
2019-05-26 18:58:47 +0900

    keepにするメソッド追加

--- app/models/try_thing.rb
+++ app/models/try_thing.rb
@@ -2,4 +2,11 @@ class TryThing < ApplicationRecord
   belongs_to :problem_thing, optional: true
 
   validates :content, presence: true
+
+  def create_keep_things_from_try_thing!
+    transaction do
+      KeepThing.create!(content: content, kpt: problem_thing.kpt)
+      self.update!(complete: true)
+    end
+  end
 end
