c152f52e8ddb1c914a0f5db6e390f0e0145e6e9e
daikitakaya <takaya@sonicgarden.jp>
2019-05-27 19:41:30 +0900

    メーラーテスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :project_memberships, dependent: :destroy
   has_many :projects, through: :project_memberships
   has_many :kpts, through: :projects, source: :kpts
--- spec/features/registrations_spec.rb
+++ spec/features/registrations_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 
 feature 'Registration' do
   feature 'ユーザー登録' do
+    let(:mail) { open_email('sample@example.com') }
     it 'ユーザー登録ができること' do
       visit new_user_registration_path
       fill_in 'Eメール', with: 'sample@example.com'
       fill_in 'パスワード', with: '12345678'
       fill_in 'user[password_confirmation]', with: '12345678'
       click_on 'Sign up'
-      expect(page).to have_content 'アカウント登録が完了しました'
+      expect(mail).to have_body_text(/以下のリンクをクリックし、メールアドレスの確認手続を完了させてください。/)
+      click_email_link_matching(/\/users\/confirmation/, mail)
+      expect(page).to have_content /アカウントを登録しました。/
     end
   end
 end
