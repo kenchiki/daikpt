02c8d4ec5b2bce01c9a5841050eaf280413389a8
daikitakaya <takaya@sonicgarden.jp>
2019-05-27 19:51:32 +0900

    userノファクトリにconfirmed_at追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:name) { Faker::Name.name }
     sequence(:email) { |n| "test#{n}@example.com" }
     password { '12345678' }
+    confirmed_at { Time.current }
   end
 
   trait :with_project do
